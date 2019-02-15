09a78cc4b71d859bbbd1d8a99a1b0956e97a8973
Andrew Johnson <ajohnson400@gatech.edu>
2019-02-15 11:11:35 -0500

    Script to run all input files
    Requires two environment variables:
    - SSS2_EXE - path to serpent executable
    - SERPENT_DATA - path to SERPENT xs libraries
    
    Can be tweaked with by changing SSS2_OPTS variable, used as
    $SSS2_EXE $SSS2_OPTS $ff
    for file ff

---
+++ runAll.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Run all input files
+
+START="$(pwd)"
+FILES=(simple det/det dep/dep history/hist)
+SSS2_OPTS="-noplot -omp 2"
+
+if [ -z $SSS2_EXE ]; then
+    echo "Export SSS2_EXE variable"
+    exit 1
+fi
+
+if [ -z $SERPENT_DATA ]; then
+    echo "Export SERPENT_DATA variable"
+    exit 1
+fi
+
+echo $START
+
+for ff in ${FILES[*]}; do
+    echo $ff
+    cd $(dirname $ff)
+    
+    $SSS2_EXE $SSS2_OPTS $(basename $ff)
+
+    cd $START
+done
