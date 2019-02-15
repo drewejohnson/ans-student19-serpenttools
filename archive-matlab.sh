6b3247960e6e91fa76663b0199634e59a5d4e736
Andrew Johnson <ajohnson400@gatech.edu>
2019-02-15 10:21:05 -0500

    Bash script to archive all matlab files

---
+++ archive-matlab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Archive all matlab and coefficient files from runs
+
+FILES=$(find . -name "*.m")
+echo $FILES
+zip files.zip $FILES
+tar -czf files.tgz $FILES
+# sha256 and md5 sums
+sha256sum files.zip files.tgz > files.sha256
+md5sum files.zip files.tgz > files.md5
