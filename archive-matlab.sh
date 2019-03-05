d05878d0faa9e52bf270ca9cc132dade33efb6df
Andrew Johnson <ajohnson400@gatech.edu>
2019-03-05 14:50:27 -0500

    Update files archived with tar

--- archive-matlab.sh
+++ archive-matlab.sh
@@ -3,7 +3,7 @@
 
 FILES=$(find . -name "*.m" -o -name "*.coe")
 zip files.zip $FILES
-tar -czf files.tgz $FILES
+tar -cvzf files.tgz $FILES
 # sha256 and md5 sums
 sha256sum files.zip files.tgz > files.sha256
 md5sum files.zip files.tgz > files.md5
