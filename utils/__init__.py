774b907b8dc0954cc16c089b37db621445d54868
Andrew Johnson <drewej@protonmail.com>
2019-03-25 08:32:23 -0400

    Remove utils/coe2Frame.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,16 +1,4 @@
 from matplotlib.pyplot import pcolormesh
-from .coe2frame import buildFrame
-
-def setupCollector(col):
-    if not col.xsTables:
-        col.collect()
-    col.states = (
-        (300., 750.),
-        (300., 600.),
-        (1200., 300., 900.,),
-    )
-    col.axis = ("Univ", "Coolant Dens", "Coolant Temp", "Fuel Temp", "Burnup", "Group")
-
 
 def plotDepmtxStructure(mtx):
     orig = mtx.data.copy()
