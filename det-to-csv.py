3d134d6a0ef4bfef118554611a2b55dac15a7397
Andrew Johnson <drewej@protonmail.com>
2019-03-25 12:27:29 -0400

    Use builtin detector file for csv export

--- det-to-csv.py
+++ det-to-csv.py
@@ -1,7 +1,7 @@
 from numpy import empty
 from itertools import product
 import serpentTools
-d = serpentTools.readDataFile('bwr_det0.m')['xymesh']
+d = serpentTools.read('det_det0.m')['xymesh']
 
 # Get data
 
@@ -11,17 +11,15 @@ ygrids = d.grids['Y']
 nx = xgrids.shape[0]
 ny = ygrids.shape[0]
 
-data = empty((nx * ny, 5))
+data = empty((nx * ny, 4))
 data[:, 2].fill(0)
 
 for ix, (yi, xi) in enumerate(product(range(ny), range(nx))):
-    data[ix, 0] = xgrids[xi, 1]
-    data[ix, 1] = ygrids[yi, 1]
-    data[ix, 3] = d.tallies[0, yi, xi]
-    data[ix, 4] = d.tallies[1, yi, xi]
+    data[ix, 0] = xgrids[xi, 2]
+    data[ix, 1] = ygrids[yi, 2]
+    data[ix, 3] = d.tallies[yi, xi]
 
-with open('bwr.csv', 'w') as out:
-    out.write("X Axis,Y Axis,Z Axis,Thermal,Fast\n")
+with open('det.csv', 'w') as out:
+    out.write("X Axis,Y Axis,Z Axis,Flux\n")
     for row in data:
         out.write(','.join(map(str, row)) + "\n")
-
